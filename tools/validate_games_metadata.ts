4592a0930d8f4729bb9331af74ae2ed854a5759e
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2024-11-16 19:44:20 +0200

    Tools: Implement Games Metadata JSON validator

---
+++ tools/validate_games_metadata.ts
@@ -0,0 +1,49 @@
+import Ajv from "ajv/dist/2020";
+import { readdir } from "node:fs/promises";
+
+const ajv = new Ajv();
+
+(async () => {
+	const metadataScheme = await Bun.file("./shared/metadata_scheme.json").json();
+
+	const schema = metadataScheme;
+	const validate = ajv.compile(schema);
+	let errorsCount = 0;
+
+	const platformsList = await readdir("./platforms", { withFileTypes: true });
+
+	for (const platform of platformsList) {
+		if (!platform.isDirectory()) {
+			continue;
+		}
+
+		const { path, name } = platform;
+
+		const gamesFoldersList = await readdir(`${path}/${name}`, {
+			withFileTypes: true,
+		});
+
+		for (const gameFolder of gamesFoldersList) {
+			if (!gameFolder.isDirectory()) {
+				continue;
+			}
+
+			const { path: gameFolderPath, name: gameFolderName } = gameFolder;
+			const gameFolderRoute = `${gameFolderPath}/${gameFolderName}`;
+
+			const gameMetadata = await Bun.file(
+				`${gameFolderRoute}/metadata.json`,
+			).json();
+
+			const valid = validate(gameMetadata);
+
+			if (!valid) {
+				console.log(validate.errors, gameFolderRoute);
+
+				errorsCount += 1;
+			}
+		}
+	}
+
+	console.log(errorsCount);
+})();
